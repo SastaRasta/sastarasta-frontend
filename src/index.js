00a0382500abcf74a1a4c432fccd47cf84d9f670
Bharath1910 <av.bharath@proton.me>
2024-04-06 20:05:58 +0530

    completed trip suggest

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-function onClick()
-{
-    fetch("http://192.168.137.159:5000/auth/login")
-    .then(res => res.json())
-    .then(data => {
-        window.location.href = data.url
-    })
+const remoteURL = "http://localhost:5000"
 
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
+
+const login = document.getElementById("login");
+login.addEventListener("click", (e) => {
+    fetch(`${remoteURL}/auth/login`)
+        .then(res => res.json())
+        .then(data => {
+            window.location.href = data.url
+        })
+});
 
 const tripSuggestionForm = document.getElementById("trip_suggestion");
 tripSuggestionForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(tripSuggestionForm);
     const data = Object.fromEntries(formData);
-    console.log(data);
+    
+    fetch(`${remoteURL}/trip/suggest`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + getCookie("access_token")
+        },
+        body: JSON.stringify(data),
+    })
+    .then(res => {
+        if (res.ok) {
+            return res.json();
+        } else if (res.status === 401) {
+            alert("You are not authorized to perform this action. Please login.")
+        }
+    })
+    .then(data => {
+        document.getElementById('test_res').innerHTML = data
+    })
+
 });
